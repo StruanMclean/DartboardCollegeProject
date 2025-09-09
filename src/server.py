d52534ee5f42684daa692571bd228bf715ad448f
Struan Mclean <struanmclean271107@gmail.com>
2025-09-09 18:40:09 +0100

    added some basic types

--- src/server.py
+++ src/server.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from typing import List, Tuple
+from fastapi import FastAPI, WebSocket
+from src.camrea import Camera
+from src.helpers.logger import logger
 
 app = FastAPI()
+
+cam1 = Camera(0)
+cam2 = Camera(1)
 
 @app.get("/")
 async def root():
     return {"message": "Running"}
 
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    logger.info("WebSocket client connected")
+
+    global scores
+    scores: List[Tuple[int, str]] = []
+
+    while True:
+        await websocket.receive_text()
+        await websocket.send_text(f"Message text was:")
